fe82c9b21f0cfa6e62d4e4ce1f70f5bf01d3cd6b
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-10-23 19:50:23 -0700

    http response code module fixed

--- rules/httpResponseCode.js
+++ rules/httpResponseCode.js
@@ -1,17 +1,12 @@
 //If the module return true, 
-//rule is matched and logger is turned on
+//final notify callback is called
 module.exports = function() {
-    var args = Array.prototype.slice.call(arguments);
+    var args = Array.prototype.slice.call(arguments)[0];
     return function(req, res, originalArgs) {
-        var requestStatus = originalArgs[0];
+        var requestStatus = res.statusCode;
         // No args -> do nothing, else match code or range
         return (!args.length || args.some(function(range) {
-            // One array member means exact status to match
-            if (range.length === 1) {
-                return requestStatus === range[0];
-            } else if (range.length === 2) {
-                return requestStatus >= range[0] && requestStatus <= range[1];
-            }
+            return requestStatus === range;
         }));
     }
 };
